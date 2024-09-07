6dd60019086f05e02733f73f36579fb62d25154e
rishabh-ml <contact.rishabh.shukla@gmail.com>
2024-09-07 19:32:09 +0530

    First Java File Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World!! This is Rishabh.");
         System.out.println("Let's Enjoy this Journey of Java Programming Together.");
-        System.out.print("Nice to Meet U. ");
-        System.out.print("Bye");
+        System.out.println("Nice to Meet U. ");
+        System.out.print("Bye.");
     }
 }
