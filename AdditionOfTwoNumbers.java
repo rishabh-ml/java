a5da171409ac10777ecbf4ad3cecc3269de06171
rishabh-ml <contact.rishabh.gmail.com@gmail.com>
2024-09-09 01:20:11 +0530

    Nineteenth Java File

---
+++ AdditionOfTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AdditionOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        double sum = num1 + num2;
+        
+        System.out.println("The sum is: " + sum);
+        
+        scanner.close();
+    }
+}
